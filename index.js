559e4df3aa93100f770baf82162c9475ccb4e8e3
Rene Santana <renesantana10@gmail.com>
2018-08-21 01:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject.recipes
-  return recipes
+  return object
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
